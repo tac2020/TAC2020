fb4bfd3e59745dcd5207fc5781c00265adacede2
Rahul Pahuja <rahulpahuja2015@gmail.com>
2020-04-02 14:54:37 +0530

    Added Mobile Folder

---
+++ mobile/cheal/app/src/main/java/tac/india/cheal/patients/ui/LoginActivity.java
@@ -0,0 +1,16 @@
+package tac.india.cheal.patients.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import tac.india.cheal.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
